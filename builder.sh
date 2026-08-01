4214f1d4f0a0ecd099e254a80b396fefb8920d5b
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-01 07:57:13 -0400

    Create builder.sh

---
+++ builder.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This variable controls the installation prefix.
+export PREFIX="/emulator"
+
+# This variable controls the target list.
+export QEMU_TARGET_LIST="ppc-softmmu"
+
+# This variable controls the file loaded for each emulator profile. The filename is the same for all profiles, but the directories are profile-specific.
+export ARCH_DEVICE_FILE="whp-profile"
+
+# Configure QEMU based on this script's settings.
+./configure --enable-gtk --enable-slirp --prefix="$PREFIX" --target-list="$QEMU_TARGET_LIST" --without-default-devices --without-default-features --with-devices-ppc="$PPC_ARCH_DEVICE_FILE"
+make all install
