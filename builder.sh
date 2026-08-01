a3c14f061f93cc3310276d2b9f62e1329879148b
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-01 11:09:10 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,15 +2,19 @@
 
 set -e
 
+
+# This variable controls the file loaded for each emulator profile. The filename is the same for all profiles, but the directories are profile-specific.
+export ARCH_DEVICE_FILE="whp-profile"
+
+# This variable controls the C compiler flags.
+export CFLAGS="-g0 -pipe -w"
+
 # This variable controls the installation prefix.
 export PREFIX="/emulator"
 
 # This variable controls the target list.
 export QEMU_TARGET_LIST="ppc-softmmu"
 
-# This variable controls the file loaded for each emulator profile. The filename is the same for all profiles, but the directories are profile-specific.
-export ARCH_DEVICE_FILE="whp-profile"
-
 # Configure QEMU based on this script's settings.
-./configure --enable-gtk --enable-pixman --enable-pulseaudio --enable-rng-none --enable-slirp --prefix="$PREFIX" --target-list="$QEMU_TARGET_LIST" --without-default-devices --without-default-features --with-devices-ppc="$ARCH_DEVICE_FILE"
+./configure --enable-gtk --enable-pixman --enable-pa --enable-rng-none --enable-slirp --prefix="$PREFIX" --target-list="$QEMU_TARGET_LIST" --without-default-devices --without-default-features --with-devices-ppc="$ARCH_DEVICE_FILE"
 make all install
