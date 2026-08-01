dbfd8d188130e89bf582463c677d1a711e2c2e8e
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-01 17:31:15 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -7,13 +7,16 @@ set -e
 export ARCH_DEVICE_FILE="whp-profile"
 
 # This variable controls the C compiler flags.
-export CFLAGS="-g0 -pipe -w"
+export CFLAGS="-g0 -march=native -mtune=native -pipe -w"
+
+# This variable controls the C++ compiler flags.
+export CXXFLAGS="-g0 -P -pipe -w"
 
 # This variable controls the installation prefix.
 export PREFIX="/emulator"
 
 # This variable controls the target list.
-export QEMU_TARGET_LIST="ppc-softmmu"
+export QEMU_TARGET_LIST="i386-softmmu,ppc-softmmu"
 
 # Configure QEMU based on this script's settings.
 ./configure --enable-gtk --enable-pixman --enable-pa --enable-rng-none --enable-slirp --enable-tools --prefix="$PREFIX" --target-list="$QEMU_TARGET_LIST" --without-default-devices --without-default-features --with-devices-ppc="$ARCH_DEVICE_FILE"
