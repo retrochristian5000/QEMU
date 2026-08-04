53830f5c6cb814b5b01d750e078944d3e95bf896
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-04 15:11:53 -0400

    Pass active build identity to machine verifier

--- builder.sh
+++ builder.sh
@@ -22,4 +22,7 @@ bash "$SOURCE_DIR/scripts/verify-macos-gtk.sh"
 whp_prepare_sources
 whp_configure_build
 whp_build_targets "$@"
-bash "$SOURCE_DIR/scripts/verify-qemu-machine-profiles.sh" "$@"
+BUILD_DIR="$BUILD_DIR" \
+QEMU_TARGET_LIST="$QEMU_TARGET_LIST" \
+BUILD_TARGETS="$BUILD_TARGETS" \
+    bash "$SOURCE_DIR/scripts/verify-qemu-machine-profiles.sh" "$@"
