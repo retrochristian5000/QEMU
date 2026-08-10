9edc28515e42fda3d54d41f331d9323e53224fee
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 18:15:17 -0400

    hw/i386: fix Weitek 4167 IRQ and address types

--- include/hw/i386/weitek4167.h
+++ include/hw/i386/weitek4167.h
@@ -7,6 +7,7 @@
 #ifndef HW_I386_WEITEK4167_H
 #define HW_I386_WEITEK4167_H
 
+#include "hw/core/irq.h"
 #include "hw/core/sysbus.h"
 
 #define TYPE_WEITEK4167 "weitek4167"
@@ -16,10 +17,12 @@ OBJECT_DECLARE_SIMPLE_TYPE(Weitek4167State, WEITEK4167)
  * The 4167 is selected in the 0xc0000000-0xc1ffffff physical range.
  * A24..A16 are not part of the coprocessor's operation address input, so
  * the 64 KiB programming window is mirrored throughout that decode range.
+ * Keep physical address constants typed as hwaddr so HWADDR_PRIx users do not
+ * inherit the host C type of an integer-literal suffix.
  */
-#define WEITEK4167_MMIO_BASE        0xc0000000ULL
-#define WEITEK4167_MMIO_SIZE        0x02000000ULL
-#define WEITEK4167_PROGRAM_SIZE     0x00010000ULL
+#define WEITEK4167_MMIO_BASE        ((hwaddr)0xc0000000)
+#define WEITEK4167_MMIO_SIZE        ((hwaddr)0x02000000)
+#define WEITEK4167_PROGRAM_SIZE     ((hwaddr)0x00010000)
 #define WEITEK4167_PROGRAM_MASK     (WEITEK4167_PROGRAM_SIZE - 1)
 
 /* AT-compatible systems route the coprocessor interrupt output to IRQ13. */
