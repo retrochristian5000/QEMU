370ffabade65fd7aee890f186cdc981ccbd8ec5b
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 05:48:24 -0400

    hw/audio: add CS4630 device interface

---
+++ include/hw/audio/cs4630.h
@@ -0,0 +1,15 @@
+/*
+ * Cirrus Logic Crystal SoundFusion CS4630
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_AUDIO_CS4630_H
+#define HW_AUDIO_CS4630_H
+
+#include "qom/object.h"
+
+#define TYPE_CS4630 "cs4630"
+OBJECT_DECLARE_SIMPLE_TYPE(CS4630State, CS4630)
+
+#endif /* HW_AUDIO_CS4630_H */
