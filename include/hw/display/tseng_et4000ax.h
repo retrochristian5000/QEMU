465c0b6664016e9ca808ad114639c7370251d308
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 05:26:00 -0400

    hw/display: add Tseng ET4000AX device interface

---
+++ include/hw/display/tseng_et4000ax.h
@@ -0,0 +1,12 @@
+/*
+ * Tseng Labs ET4000AX ISA VGA controller
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef HW_DISPLAY_TSENG_ET4000AX_H
+#define HW_DISPLAY_TSENG_ET4000AX_H
+
+#define TYPE_ISA_ET4000AX "isa-et4000ax"
+
+#endif
