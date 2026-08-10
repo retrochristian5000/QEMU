4ef5553dd5f374155fd7846edc8e53574944c9e8
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 10:29:14 -0400

    target: add Signetics 2650 CPU parameters

---
+++ target/s2650/cpu-param.h
@@ -0,0 +1,14 @@
+/*
+ * Signetics 2650 CPU parameters
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef S2650_CPU_PARAM_H
+#define S2650_CPU_PARAM_H
+
+/* 32 KiB linear architectural address space. */
+#define TARGET_PAGE_BITS 8
+#define TARGET_VIRT_ADDR_SPACE_BITS 15
+
+#endif /* S2650_CPU_PARAM_H */
