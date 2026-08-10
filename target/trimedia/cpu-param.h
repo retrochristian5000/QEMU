e9c9d1662de5804adaa648e580d0f3ed77bf7253
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 07:33:41 -0400

    target/trimedia: define CPU parameters

---
+++ target/trimedia/cpu-param.h
@@ -0,0 +1,15 @@
+/*
+ * TriMedia CPU parameters
+ *
+ * Copyright (c) 2026
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ */
+
+#ifndef TRIMEDIA_CPU_PARAM_H
+#define TRIMEDIA_CPU_PARAM_H
+
+#define TARGET_PAGE_BITS 12
+#define TARGET_VIRT_ADDR_SPACE_BITS 32
+
+#endif
