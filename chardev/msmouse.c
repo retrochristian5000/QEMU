4ad8bd087157b77df43e250034330a09ed2c2445
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-08-10 10:56:41 -0400

    chardev: add Microsoft EasyBall serial mouse profile

--- chardev/msmouse.c
+++ chardev/msmouse.c
@@ -40,23 +40,68 @@
 /* Serial fifo size. */
 #define MSMOUSE_BUF_SZ 64
 
-/* Mouse ID: Send "M3" cause we behave like a 3 button logitech mouse. */
-const uint8_t mouse_id[] = {'M', '3'};
+/* Existing QEMU mouse ID: three-button Logitech-compatible extension. */
+static const uint8_t msmouse_id[] = {'M', '3'};
+/* Microsoft EasyBall uses the base Microsoft serial-mouse identification. */
+static const uint8_t easyball_id[] = {'M'};
+
 /*
  * PnP start "(", PnP version (1.0), vendor ID, product ID, '\\',
  * serial ID (omitted), '\\', MS class name, '\\', driver ID (omitted), '\\',
  * product description, checksum, ")"
  * Missing parts are inserted later.
  */
-const uint8_t pnp_data[] = {M('('), 1, '$', M('Q'), M('M'), M('U'),
-                         M('0'), M('0'), M('0'), M('1'),
-                         M('\\'), M('\\'),
-                         M('M'), M('O'), M('U'), M('S'), M('E'),
-                         M('\\'), M('\\')};
+static const uint8_t msmouse_pnp_data[] = {
+    M('('), 1, '$', M('Q'), M('M'), M('U'),
+    M('0'), M('0'), M('0'), M('1'),
+    M('\\'), M('\\'),
+    M('M'), M('O'), M('U'), M('S'), M('E'),
+    M('\\'), M('\\')
+};
+
+/* PNP0F1E is the Microsoft Kids Trackball / Serial EasyBall identifier. */
+static const uint8_t easyball_pnp_data[] = {
+    M('('), 1, '$', M('P'), M('N'), M('P'),
+    M('0'), M('F'), M('1'), M('E'),
+    M('\\'), M('\\'),
+    M('M'), M('O'), M('U'), M('S'), M('E'),
+    M('\\'), M('\\')
+};
+
+typedef struct MouseProfile {
+    const uint8_t *mouse_id;
+    size_t mouse_id_len;
+    const uint8_t *pnp_data;
+    size_t pnp_data_len;
+    const char *description;
+    bool right_button;
+    bool middle_button;
+} MouseProfile;
+
+static const MouseProfile msmouse_profile = {
+    .mouse_id = msmouse_id,
+    .mouse_id_len = sizeof(msmouse_id),
+    .pnp_data = msmouse_pnp_data,
+    .pnp_data_len = sizeof(msmouse_pnp_data),
+    .description = "QEMU Microsoft Mouse",
+    .right_button = true,
+    .middle_button = true,
+};
+
+static const MouseProfile easyball_profile = {
+    .mouse_id = easyball_id,
+    .mouse_id_len = sizeof(easyball_id),
+    .pnp_data = easyball_pnp_data,
+    .pnp_data_len = sizeof(easyball_pnp_data),
+    .description = "Microsoft Serial EasyBall",
+    .right_button = false,
+    .middle_button = false,
+};
 
 struct MouseChardev {
     Chardev parent;
 
+    const MouseProfile *profile;
     QemuInputHandlerState *hs;
     int tiocm;
     int axis[INPUT_AXIS__MAX];
@@ -67,6 +112,7 @@ struct MouseChardev {
 typedef struct MouseChardev MouseChardev;
 
 #define TYPE_CHARDEV_MSMOUSE "chardev-msmouse"
+#define TYPE_CHARDEV_EASYBALL "chardev-easyball"
 DECLARE_INSTANCE_CHECKER(MouseChardev, MOUSE_CHARDEV,
                          TYPE_CHARDEV_MSMOUSE)
 
@@ -105,10 +151,13 @@ static void msmouse_queue_event(MouseChardev *mouse)
     bytes[2] |= MSMOUSE_LO6(dy);
 
     /* Buttons */
-    bytes[0] |= (mouse->btns[INPUT_BUTTON_LEFT]   ? 0x20 : 0x00);
-    bytes[0] |= (mouse->btns[INPUT_BUTTON_RIGHT]  ? 0x10 : 0x00);
-    if (mouse->btns[INPUT_BUTTON_MIDDLE] ||
-        mouse->btnc[INPUT_BUTTON_MIDDLE]) {
+    bytes[0] |= (mouse->btns[INPUT_BUTTON_LEFT] ? 0x20 : 0x00);
+    if (mouse->profile->right_button) {
+        bytes[0] |= (mouse->btns[INPUT_BUTTON_RIGHT] ? 0x10 : 0x00);
+    }
+    if (mouse->profile->middle_button &&
+        (mouse->btns[INPUT_BUTTON_MIDDLE] ||
+         mouse->btnc[INPUT_BUTTON_MIDDLE])) {
         bytes[3] |= (mouse->btns[INPUT_BUTTON_MIDDLE] ? 0x20 : 0x00);
         mouse->btnc[INPUT_BUTTON_MIDDLE] = false;
         count++;
@@ -137,6 +186,12 @@ static void msmouse_input_event(DeviceState *dev, QemuConsole *src,
         break;
 
     case INPUT_EVENT_KIND_BTN:
+        if ((!mouse->profile->right_button &&
+             evt->btn.button == INPUT_BUTTON_RIGHT) ||
+            (!mouse->profile->middle_button &&
+             evt->btn.button == INPUT_BUTTON_MIDDLE)) {
+            return;
+        }
         mouse->btns[evt->btn.button] = evt->btn.down;
         mouse->btnc[evt->btn.button] = true;
         break;
@@ -174,15 +229,43 @@ static const QemuInputHandler msmouse_handler = {
     .sync  = msmouse_input_sync,
 };
 
+static void msmouse_queue_identification(MouseChardev *mouse)
+{
+    const MouseProfile *profile = mouse->profile;
+    int c, i, j;
+    uint8_t bytes[MSMOUSE_BUF_SZ / 2];
+    const uint8_t hexchr[16] = {
+        M('0'), M('1'), M('2'), M('3'), M('4'), M('5'), M('6'), M('7'),
+        M('8'), M('9'), M('A'), M('B'), M('C'), M('D'), M('E'), M('F')
+    };
+
+    g_assert(strlen(profile->description) + 3 <= sizeof(bytes));
+    g_assert(profile->mouse_id_len + profile->pnp_data_len +
+             strlen(profile->description) + 3 <= MSMOUSE_BUF_SZ);
+
+    fifo8_push_all(&mouse->outbuf, profile->mouse_id, profile->mouse_id_len);
+    fifo8_push_all(&mouse->outbuf, profile->pnp_data, profile->pnp_data_len);
+
+    c = M(')');
+    for (i = 0; profile->description[i]; i++) {
+        bytes[i] = M(profile->description[i]);
+        c += bytes[i];
+    }
+    for (j = 0; j < profile->pnp_data_len; j++) {
+        c += profile->pnp_data[j];
+    }
+    c &= 0xff;
+    bytes[i++] = hexchr[c >> 4];
+    bytes[i++] = hexchr[c & 0x0f];
+    bytes[i++] = M(')');
+    fifo8_push_all(&mouse->outbuf, bytes, i);
+}
+
 static int msmouse_chr_ioctl(Chardev *chr, int cmd, void *arg)
 {
     MouseChardev *mouse = MOUSE_CHARDEV(chr);
-    int c, i, j;
-    uint8_t bytes[MSMOUSE_BUF_SZ / 2];
+    int c;
     int *targ = (int *)arg;
-    const uint8_t hexchr[16] = {M('0'), M('1'), M('2'), M('3'), M('4'), M('5'),
-                             M('6'), M('7'), M('8'), M('9'), M('A'), M('B'),
-                             M('C'), M('D'), M('E'), M('F')};
 
     switch (cmd) {
     case CHR_IOCTL_SERIAL_SET_TIOCM:
@@ -191,30 +274,10 @@ static int msmouse_chr_ioctl(Chardev *chr, int cmd, void *arg)
         if (MSMOUSE_PWR(mouse->tiocm)) {
             if (!MSMOUSE_PWR(c)) {
                 /*
-                 * Power on after reset: Send ID and PnP data
-                 * No need to check fifo space as it is empty at this point.
+                 * Power on after reset: Send ID and PnP data.
+                 * The profile payload is guaranteed to fit in an empty FIFO.
                  */
-                fifo8_push_all(&mouse->outbuf, mouse_id, sizeof(mouse_id));
-                /* Add PnP data: */
-                fifo8_push_all(&mouse->outbuf, pnp_data, sizeof(pnp_data));
-                /*
-                 * Add device description from qemu handler name.
-                 * Make sure this all fits into the queue beforehand!
-                 */
-                c = M(')');
-                for (i = 0; msmouse_handler.name[i]; i++) {
-                    bytes[i] = M(msmouse_handler.name[i]);
-                    c += bytes[i];
-                }
-                /* Calc more of checksum */
-                for (j = 0; j < sizeof(pnp_data); j++) {
-                    c += pnp_data[j];
-                }
-                c &= 0xff;
-                bytes[i++] = hexchr[c >> 4];
-                bytes[i++] = hexchr[c & 0x0f];
-                bytes[i++] = M(')');
-                fifo8_push_all(&mouse->outbuf, bytes, i);
+                msmouse_queue_identification(mouse);
                 /* Start sending data to serial. */
                 msmouse_chr_accept_input(chr);
             }
@@ -255,6 +318,7 @@ static bool msmouse_chr_open(Chardev *chr,
 {
     MouseChardev *mouse = MOUSE_CHARDEV(chr);
 
+    g_assert(mouse->profile != NULL);
     mouse->hs = qemu_input_handler_register((DeviceState *)mouse,
                                             &msmouse_handler);
     mouse->tiocm = 0;
@@ -262,6 +326,20 @@ static bool msmouse_chr_open(Chardev *chr,
 
     /* Never send CHR_EVENT_OPENED */
     return true;
+}
+
+static void char_msmouse_init(Object *obj)
+{
+    MouseChardev *mouse = MOUSE_CHARDEV(obj);
+
+    mouse->profile = &msmouse_profile;
+}
+
+static void char_easyball_init(Object *obj)
+{
+    MouseChardev *mouse = MOUSE_CHARDEV(obj);
+
+    mouse->profile = &easyball_profile;
 }
 
 static void char_msmouse_class_init(ObjectClass *oc, const void *data)
@@ -278,13 +356,21 @@ static const TypeInfo char_msmouse_type_info = {
     .name = TYPE_CHARDEV_MSMOUSE,
     .parent = TYPE_CHARDEV,
     .instance_size = sizeof(MouseChardev),
+    .instance_init = char_msmouse_init,
     .instance_finalize = char_msmouse_finalize,
     .class_init = char_msmouse_class_init,
+};
+
+static const TypeInfo char_easyball_type_info = {
+    .name = TYPE_CHARDEV_EASYBALL,
+    .parent = TYPE_CHARDEV_MSMOUSE,
+    .instance_init = char_easyball_init,
 };
 
 static void register_types(void)
 {
     type_register_static(&char_msmouse_type_info);
+    type_register_static(&char_easyball_type_info);
 }
 
 type_init(register_types);
