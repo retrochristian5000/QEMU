fa64ce3dd098429cac50d1771174d0baca731a54
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-07 04:36:43 -0400

    pci: define PM capability version 1.0

--- include/hw/pci/pci_regs.h
+++ include/hw/pci/pci_regs.h
@@ -3,6 +3,7 @@
 
 #include "standard-headers/linux/pci_regs.h"
 
+#define  PCI_PM_CAP_VER_1_0     0x0001  /* PCI PM spec ver. 1.0 */
 #define  PCI_PM_CAP_VER_1_1     0x0002  /* PCI PM spec ver. 1.1 */
 #define  PCI_PM_CAP_VER_1_2     0x0003  /* PCI PM spec ver. 1.2 */
 
