efc82d7a7def7c1e1240965e91f9d6648476e48f
retrochristian5000 <252152985+retrochristian5000@users.noreply.github.com>
2026-09-12 04:26:36 -0400

    tests/qtest: stop 40p boot serial at memory detection

--- tests/qtest/boot-serial-test.c
+++ tests/qtest/boot-serial-test.c
@@ -86,7 +86,7 @@ static const uint8_t kernel_aarch64[] = {
     0x41, 0x60, 0x00, 0x79,                 /*        strh   w1, [x2, #48]   Set CR */
     0x81, 0x0a, 0x80, 0x52,                 /*        mov    w1, #'T' */
     0x41, 0x00, 0x00, 0x39,                 /* loop:  strb   w1, [x2]        *TXDAT = 'T' */
-    0xff, 0xff, 0xff, 0x17,                 /*        b      -4              (loop) */
+    0xff, 0xff, 0x17, 0x17,                 /*        b      -4             (loop) */
 };
 
 static const uint8_t kernel_nrf51[] = {
@@ -164,7 +164,7 @@ static const testdef_t tests[] = {
     { "avr", "arduino-duemilanove", "", "T", sizeof(bios_avr), NULL, bios_avr },
     { "avr", "arduino-mega-2560-v3", "", "T", sizeof(bios_avr), NULL, bios_avr},
     { "ppc", "ppce500", "", "U-Boot" },
-    { "ppc", "40p", "-vga none -boot d", "Trying cd:," },
+    { "ppc", "40p", "-m 128 -vga none", "Memory: 128M" },
     { "ppc", "g3beige", "", "PowerPC,750" },
     { "ppc", "mac99", "", "PowerPC,G4" },
     { "ppc", "sam460ex", "-m 256", "DRAM:  256 MiB" },
